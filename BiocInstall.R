201b3c47a56dd907583f516f670c55f932a7de69
wilsontom <wilsontom89@gmail.com>
2021-03-08 16:30:37 +0000

    install R packages

---
+++ BiocInstall.R
@@ -0,0 +1,8 @@
+#!/usr/bin/R
+
+BiocManager::install(version = 'devel')
+BiocManager::install('mzR')
+BiocManager::install('MSnbase')
+BiocManager::install('xcms')
+BiocManager::install('SummarizedExperiments')
+BiocManager::install('MsFeatures')
